cdc2e64d11d1a66a869a6cfe0d3691c25fc0d80e
Lowell Scott <blaarg171@gmail.com>
2017-02-11 09:32:06 -0600

    Implemented level manager

---
+++ Break the Ice/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    //public GameObject PlayerPrefab;
+    public GameObject PlatformPrefab;
+    public List<GameObject> Platforms;
+    public GameObject EnemyPrefab;
+    public List<GameObject> Enemies;
+    public GameObject[] OtherPrefabs;
+    public List<GameObject> Others;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Level 1
+        Platforms.Add(Instantiate(PlatformPrefab, new Vector3(0, -4.75f, 0), Quaternion.identity));
+        Platforms.Add(Instantiate(PlatformPrefab, new Vector3(8.5f, -3.5f, 0), Quaternion.identity));
+        Platforms.Add(Instantiate(PlatformPrefab, new Vector3(14.6f, -4.4f, 0), Quaternion.identity));
+        Platforms.Add(Instantiate(PlatformPrefab, new Vector3(18.8f, -3.3f, 0), Quaternion.identity));
+        Platforms[3].transform.localScale = new Vector3(0.875f, 1, 1);
+        Enemies.Add(CreateFloorEnemy());
+        Enemies.Add(Instantiate(EnemyPrefab, new Vector3(7f, -3f, 0), Quaternion.identity));
+        Others.Add(Instantiate(OtherPrefabs[0], new Vector3(18.89f, -2.74f, 0), Quaternion.identity));
+
+        foreach (GameObject igo in Platforms)
+            igo.transform.SetParent(this.transform);
+        foreach (GameObject igo in Enemies)
+            igo.transform.SetParent(this.transform);
+        foreach (GameObject igo in Others)
+            igo.transform.SetParent(this.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private GameObject CreateFloorEnemy()
+    {
+        GameObject go = Instantiate(EnemyPrefab);
+        go.transform.position = new Vector3(0, -6f, 0);
+        go.transform.localScale = new Vector3(200, 1, 1);
+        go.GetComponent<Rigidbody2D>().isKinematic = true;
+        return go;
+    }
+}
--- Break the Ice/Assets/Scripts/MovementManager.cs
+++ Break the Ice/Assets/Scripts/MovementManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MovementManager : MonoBehaviour
 {
     public GameObject Player;
-	public GameObject Camera;
+    public GameObject Camera;
     public float Speed;
 
     // Use this for initialization
@@ -19,25 +19,26 @@ public class MovementManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		// Use this to figure out which keys for controller support.
-		/*foreach(KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
-		{
-			if (Input.GetKeyDown(kcode))
-				Debug.Log("KeyCode down: " + kcode);
-		}*/
+        // Use this to figure out which keys for controller support.
+        /*foreach(KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
+                Debug.Log("KeyCode down: " + kcode);
+        }*/
 
-		if (Input.GetKeyDown(KeyCode.Space) && Player.GetComponent<PlayerScript>().canJump )
+        if (Input.GetKeyDown(KeyCode.Space) && Player.GetComponent<PlayerScript>().canJump)
         {
             Player.GetComponent<Rigidbody2D>().AddForce(transform.up * Speed, ForceMode2D.Impulse);
-			Player.GetComponent<PlayerScript>().canJump = false;
+            Player.GetComponent<PlayerScript>().canJump = false;
         }
 
-		if (Input.GetKeyDown (KeyCode.LeftShift)) {
-			Debug.Log ("Punch");
-		}
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("Punch");
+        }
 
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         Player.transform.position += movement / 10;
-		Camera.transform.position = new Vector3 (Player.transform.position.x, Camera.transform.position.y, Camera.transform.position.z);
+        Camera.transform.position = new Vector3(Player.transform.position.x, Camera.transform.position.y, Camera.transform.position.z);
     }
 }
