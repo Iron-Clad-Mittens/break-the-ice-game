c376add138ed7cfddd5d0874f586cc0d0d50b6ad
Ashley Coleman <ashley78@ksu.edu>
2017-02-11 02:33:52 -0600

    Add various things. Simple intro screen. Failed to fixed multiple jumps. Camera feels more natural now.

---
+++ Break the Ice/Assets/Scripts/AlphaAnimator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaAnimator : MonoBehaviour {
+
+	public Renderer renderer;
+	public float speed = .01f;
+
+	private bool isComplete = false;
+	private float time = 0; 
+	// Use this for initialization
+	void Start () {
+		Color color = renderer.material.color;
+		color.a = 0;
+		renderer.material.color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isComplete) {
+			time += Time.deltaTime;
+			if (time > 2) {
+				Application.LoadLevel ("MainScene");
+			}
+			return;
+		}
+
+		Color color = renderer.material.color;
+		color.a += speed;
+
+		if (color.a >= 1) {
+			isComplete = true;
+		}
+			
+		renderer.material.color = color;
+	}
+}
---
+++ Break the Ice/Assets/Scripts/MovementManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    public GameObject Player;
+	public GameObject Camera;
+    public float Speed;
+    private bool isGrounded = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        Player = Instantiate(Player);
+        Player.transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded )
+        {
+            Player.GetComponent<Rigidbody2D>().AddForce(transform.up * Speed, ForceMode2D.Impulse);
+			isGrounded = false;
+        }
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        Player.transform.position += movement / 10;
+		Camera.transform.position = new Vector3 (Player.transform.position.x, Camera.transform.position.y, Camera.transform.position.z);
+    }
+
+	void FixedUpdate ()
+	{
+		isGrounded = true;
+		return;
+
+		Vector3 plPosition = Player.transform.position;
+		RaycastHit2D hit = Physics2D.Raycast(new Vector3(plPosition.x, plPosition.y - 1, plPosition.z), -transform.up, 0);
+
+		if (!isGrounded && hit.collider != null && hit.collider.gameObject.name == "Platform") {
+			isGrounded = true;
+			Debug.Log (hit.distance);
+		}
+
+		if (!isGrounded)
+			return;
+		
+		//Debug.Log (hit.collider.gameObject.name);
+		//Debug.Log (hit.distance);
+	}
+}
