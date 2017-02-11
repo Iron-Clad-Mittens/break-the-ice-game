809030e52b2aa5314a08ccf4c4a6d24487588925
Ashley Coleman <ashley78@ksu.edu>
2017-02-11 02:49:54 -0600

    Fix jump thingy, begin enemy player

--- Break the Ice/Assets/Scripts/MovementManager.cs
+++ Break the Ice/Assets/Scripts/MovementManager.cs
@@ -7,7 +7,6 @@ public class MovementManager : MonoBehaviour
     public GameObject Player;
 	public GameObject Camera;
     public float Speed;
-    private bool isGrounded = false;
 
     // Use this for initialization
     void Start()
@@ -21,33 +20,14 @@ public class MovementManager : MonoBehaviour
     {
 
 
-		if (Input.GetKeyDown(KeyCode.Space) && isGrounded )
+		if (Input.GetKeyDown(KeyCode.Space) && Player.GetComponent<PlayerScript>().canJump )
         {
             Player.GetComponent<Rigidbody2D>().AddForce(transform.up * Speed, ForceMode2D.Impulse);
-			isGrounded = false;
+			Player.GetComponent<PlayerScript>().canJump = false;
         }
+
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         Player.transform.position += movement / 10;
 		Camera.transform.position = new Vector3 (Player.transform.position.x, Camera.transform.position.y, Camera.transform.position.z);
     }
-
-	void FixedUpdate ()
-	{
-		isGrounded = true;
-		return;
-
-		Vector3 plPosition = Player.transform.position;
-		RaycastHit2D hit = Physics2D.Raycast(new Vector3(plPosition.x, plPosition.y - 1, plPosition.z), -transform.up, 0);
-
-		if (!isGrounded && hit.collider != null && hit.collider.gameObject.name == "Platform") {
-			isGrounded = true;
-			Debug.Log (hit.distance);
-		}
-
-		if (!isGrounded)
-			return;
-		
-		//Debug.Log (hit.collider.gameObject.name);
-		//Debug.Log (hit.distance);
-	}
 }
---
+++ Break the Ice/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public bool canJump = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D colli){
+		if (colli.gameObject.tag == "Platform") {
+			canJump = true;
+		}
+	}
+}
